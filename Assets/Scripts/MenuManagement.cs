1b0e6faa06520502d39245b6370399c3f9554a88
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2021-11-29 20:28:22 +0100

    options_menu

--- Assets/Scripts/MenuManagement.cs
+++ Assets/Scripts/MenuManagement.cs
@@ -31,11 +31,51 @@ public class MenuManagement : MonoBehaviour
 
     [Space(10)]
     //PROFILE
-    public Image[] characters;
+    public GameObject[] characters;
+    public int selectedCharacter = 0;
     public void NextCharacter()
     {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter = (selectedCharacter + 1) % characters.Length;
+        characters[selectedCharacter].SetActive(true);
+    }
+    public void PreviousCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if(selectedCharacter < 0)
+        {
+            selectedCharacter += characters.Length;
+        }
+        characters[selectedCharacter].SetActive(true);
+    }
+    public void ChooseCharacter()
+    {
+        PlayerPrefs.SetInt("Selected Character", selectedCharacter);
+    }
+
+    [Space(10)]
+    //OPTIONS
+    public Button audioButton;
+    public Button controlsButton;
+    public GameObject audioSettingsPanel;
+    public GameObject controlsSettingsPanel;
+    public void EnableOptionsPanel(GameObject panelToEnable)
+    {
+
+        if (panelToEnable == audioSettingsPanel && !audioSettingsPanel.activeInHierarchy)
+        {
+            controlsSettingsPanel.SetActive(false);
+            audioSettingsPanel.SetActive(true);
+        }
+        else if (panelToEnable == controlsSettingsPanel && !controlsSettingsPanel.activeInHierarchy)
+        {
+            audioSettingsPanel.SetActive(false);
+            controlsSettingsPanel.SetActive(true);
+        }
         
     }
+    //------------------------------------------------------------------------
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
