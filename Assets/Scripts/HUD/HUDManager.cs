f8a1d5b83eb9281729d101dd24e3f715285f6182
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2021-12-08 08:02:41 +0100

    hud_fix

--- Assets/Scripts/HUD/HUDManager.cs
+++ Assets/Scripts/HUD/HUDManager.cs
@@ -9,7 +9,9 @@ public class HUDManager : MonoBehaviour
     Collectible collectibleObject = new Collectible();
     public Text currentScoreText;
     public static int currentScore;
-    public Animator animator;
+    public Animator dumbbleAnimator;
+    public Animator meatAnimator;
+    public Animator proteinAnimator;
 
     // Update is called once per frame
     void Update()
@@ -17,19 +19,7 @@ public class HUDManager : MonoBehaviour
         currentScoreText.text = "Score: " + currentScore.ToString();
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            Debug.Log("Hit");
-            PlayPanelAnim();
+            
         }
-    }
-
-    private void PlayPanelAnim()
-    {
-        StartCoroutine(ShowAndClose());
-    }
-    IEnumerator ShowAndClose()
-    {
-        animator.SetTrigger("Open");
-        yield return new WaitForSeconds(3f);
-        animator.SetTrigger("Close");
     }
 }
