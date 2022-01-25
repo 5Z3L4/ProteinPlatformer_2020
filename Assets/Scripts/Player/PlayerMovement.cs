42399b93cca8d4487e899da31e3b561618c58dbf
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-01-25 11:20:44 +0100

    xD

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -45,9 +45,9 @@ public class PlayerMovement : MonoBehaviour
     public bool isSliding;
     public Rigidbody2D playerRB;
     public float slideSpeed = 500;
-    private float coyoteetime = 0.1f;
+    private float coyoteetime = 0.05f;
     private float coyoteeTimeCounter;
-    private float jumpBuffer = 0.1f;
+    private float jumpBuffer = 0.05f;
     private float jumpBufferCounter;
     public CapsuleCollider2D mainCollider;
     public CircleCollider2D slideCollider;
