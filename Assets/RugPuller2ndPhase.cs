545b734b19b338f2b3f8051f728f4e066a7ea363
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-02-01 22:40:06 +0100

    rugpuller phase 2

---
+++ Assets/RugPuller2ndPhase.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RugPuller2ndPhase : StateMachineBehaviour
+{
+    private GameObject dialogue;
+    private DialogueUI dialogueUI;
+    private bool isDialogueOver;
+
+    private void Awake()
+    {
+        dialogue = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(g => g.CompareTag("BossRage"));
+        dialogueUI = GameObject.Find("Canvas").GetComponent<DialogueUI>();
+    }
+
+    //trzêœ nim
+    //odpal bubble z darciem siê
+    //odpal phase 2
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        dialogue.SetActive(true);
+        if (dialogueUI.isOver)
+        {
+            isDialogueOver = true;
+            animator.SetBool("RageRun", true);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
