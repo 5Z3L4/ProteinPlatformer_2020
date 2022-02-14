ba2df1c8a788bad0009ac9fd2c64be9f027cf4cb
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2022-02-14 06:29:20 +0100

    fixes
    -text -> white
    -"Press SPACE" tip above dialogue box
    -checkpoint perfab (4 variants)
    -scripts moved to scripts folder

---
+++ Assets/Scripts/DogDetectingPlayer.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogDetectingPlayer : MonoBehaviour
+{
+    public bool playerOnRight;
+
+    private PlayerMovement player;
+    private DogEnemy dog;
+    private LayerMask groundLayer;
+    private LayerMask playerLayer;
+    private void Awake()
+    {
+        dog = GetComponentInParent<DogEnemy>();
+        player = FindObjectOfType<PlayerMovement>();
+    }
+    private void Start()
+    {
+        groundLayer = 1 << LayerMask.NameToLayer("Ground");
+        playerLayer = 1 << LayerMask.NameToLayer("Player");
+    }
+    private void Update()
+    {
+        CheckPlayerPos(player.transform.position);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            RaycastHit2D hitGround = Physics2D.Linecast(dog.transform.position, player.transform.position, groundLayer);
+            if (hitGround.collider == null)
+            {
+                RaycastHit2D hitPlayer = Physics2D.Linecast(dog.transform.position, player.transform.position, playerLayer);
+                if (hitPlayer.collider != null)
+                {
+                    if (hitPlayer.collider.gameObject.CompareTag("Player"))
+                    {
+                        dog.shouldStartMoving = true;
+                        if (dog.facingRight != playerOnRight)
+                        {
+                            dog.Flip();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                dog.shouldStartMoving = false;
+            }
+            Debug.DrawLine(dog.transform.position, player.transform.position);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            dog.shouldStartMoving = false;
+        }
+    }
+    private void CheckPlayerPos(Vector2 playerPos)
+    {
+        if ((playerPos.x - transform.position.x) >= 0)
+        {
+            playerOnRight = true;
+        }
+        else if ((playerPos.x - transform.position.x) < 0)
+        {
+            playerOnRight = false;
+        }
+    }
+}
