e841fd0da97ba2989a7dd31e7cbc9d79fc66e7cc
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-02-20 22:22:07 +0100

    BankNote starting speed calculation + destroying

--- Assets/ProjectileFly.cs
+++ Assets/ProjectileFly.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ProjectileFly : MonoBehaviour
 {
     public float ProjectileSpeed = -250f;
+    private float _projectileSpeed;
     public LayerMask WhatIsGround;
     private PlayerMovement _player;
     private Rigidbody2D _myRb;
@@ -14,9 +15,38 @@ public class ProjectileFly : MonoBehaviour
         _myRb = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        ChoseShotingDirection();
+    }
     // Update is called once per frame
     void Update()
     {
-        _myRb.velocity = new Vector2(ProjectileSpeed * Time.deltaTime, 0);
+        _myRb.velocity = new Vector2(_projectileSpeed * Time.deltaTime, 0);
+    }
+
+    void ChoseShotingDirection()
+    {
+        if (_player.transform.position.x - transform.position.x < 0)
+        {
+            _projectileSpeed = ProjectileSpeed;
+        }
+        else
+        {
+            _projectileSpeed = -ProjectileSpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _player.TakeCertainAmountOfHp();
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.layer == WhatIsGround)
+        {
+            Destroy(gameObject);
+        }
     }
 }
