d86f4a5a4b0ba1e78158a69b7042447bbbe6d83f
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2022-02-22 18:19:42 +0100

    dog fix + lvl3 fixes

--- Assets/Scripts/Enemy/DogEnemy.cs
+++ Assets/Scripts/Enemy/DogEnemy.cs
@@ -8,7 +8,7 @@ public class DogEnemy : MonoBehaviour
 {
     public int moveSpeed;
     public int scoreValue;
-    public float stopDistance = 1.5f;
+    public float stopDistance;
     public float attackRange;
     public Transform attackPos;
     public float startTimeBtwAttack;
@@ -21,6 +21,10 @@ public class DogEnemy : MonoBehaviour
     private bool playerOnRight;
     private PlayerMovement player;
     private float timeBtwAttack = 0;
+    private void Start()
+    {
+        stopDistance = attackRange + (attackRange/2);
+    }
 
     private void Awake()
     {
@@ -85,6 +89,7 @@ public class DogEnemy : MonoBehaviour
         {
             mineRb.velocity = new Vector2(moveSpeed * Time.fixedDeltaTime, mineRb.velocity.y);
             animator.SetBool("IsWalking", true);
+            timeBtwAttack = 0;
         }
         else
         {
@@ -112,7 +117,7 @@ public class DogEnemy : MonoBehaviour
 
     private float CalculateXDistanceToPlayer()
     {
-        return Mathf.Abs(transform.position.x - player.transform.position.x);
+        return Mathf.Abs(attackPos.transform.position.x - player.transform.position.x);
     }
 
     private void OnDrawGizmosSelected()
