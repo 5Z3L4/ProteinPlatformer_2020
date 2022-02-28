95dcac904df38803b2c42ab95171e2ee9d56d06e
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-02-28 16:41:24 +0100

    minimap fix

--- Assets/MiniMap.cs
+++ Assets/MiniMap.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class MiniMap : MonoBehaviour
 {
-    public Transform objToFollow;
-    
-    void Start()
+    public Transform ObjToFollow;
+    private Camera _miniMapCam;
+
+    private void Awake()
     {
-        
+        _miniMapCam = GetComponent<Camera>();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        MapZoom();
+    }
+    private void LateUpdate()
+    {
+        FollowObj();
+    }
+    void MapZoom()
+    {
+        if (Input.GetKey(KeyCode.LeftBracket))
+        {
+            _miniMapCam.orthographicSize++;
+        }
+        else if (Input.GetKey(KeyCode.RightBracket))
+        {
+            _miniMapCam.orthographicSize--;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FollowObj()
     {
-        
+        Vector3 newPosition = ObjToFollow.position;
+        newPosition.y = transform.position.y;
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
     }
 }
