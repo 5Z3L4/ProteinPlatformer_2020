adf870bec9565893cffdb51cdcdaa4819be6ecc2
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-03-06 21:45:46 +0100

    beatcoiner v2

--- Assets/BeatCoinerGetBagUp.cs
+++ Assets/BeatCoinerGetBagUp.cs
@@ -2,35 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartBossFight : StateMachineBehaviour
+public class BeatCoinerGetBagUp : StateMachineBehaviour
 {
-    public float StopBeforePlayerDistance = 10f;
-    public float Speed = 2.5f;
-
-    private Transform _player;
-    private Rigidbody2D _rb;
+    private BeatCoinerController _beatCoiner;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("Idle", false);
-        _player = FindObjectOfType<PlayerMovement>().transform;
-        _rb = animator.GetComponent<Rigidbody2D>();
+        _beatCoiner = FindObjectOfType<BeatCoinerController>();
+        _beatCoiner.CanGetHit = true;
+        animator.SetBool("Jump", false);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Mathf.Abs(_player.position.x - _rb.position.x) < StopBeforePlayerDistance)
-        {
-            animator.SetBool("Jump", true);
-            animator.SetBool("Run", false);
-        }
-        else
-        {
-            Vector2 target = new Vector2(_player.position.x, _rb.position.y);
-            Vector2 newPos = Vector2.MoveTowards(_rb.position, target, Speed * Time.fixedDeltaTime);
-            _rb.MovePosition(newPos);
-        }
+        //liczonko
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
