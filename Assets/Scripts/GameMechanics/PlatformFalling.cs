c51cddf508d10befba10be4f67cbefab4efaea16
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-03-07 20:20:30 +0100

    falling platform fix

--- Assets/Scripts/GameMechanics/PlatformFalling.cs
+++ Assets/Scripts/GameMechanics/PlatformFalling.cs
@@ -27,7 +27,7 @@ public class PlatformFalling : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == 3)
+        if (collision.gameObject.layer != 14)
         {
             HidePlatform();
         }
