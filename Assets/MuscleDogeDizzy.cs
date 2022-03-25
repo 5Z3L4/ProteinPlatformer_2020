ca9426234debf60df467ef6d7c41c6bfec5e9974
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-03-25 23:17:02 +0100

    another adjustments doge

--- Assets/MuscleDogeDizzy.cs
+++ Assets/MuscleDogeDizzy.cs
@@ -2,50 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MuscleDogeFireworksLaunch : StateMachineBehaviour
+public class MuscleDogeDizzy : StateMachineBehaviour
 {
-    public List<GameObject> Fireworks;
-    public GameObject FinishingFirework;
-    public float StartCountTimer = 1.5f;
-    private float _countTimer = 0;
-    private int _fireworkCounter = 0;
-    private bool _lastFireWorkSpawned = false;
+    public float DizzyTimeCounter = 4f;
+    private float _dizzyTime;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        _fireworkCounter = 0;
-        _lastFireWorkSpawned = false;
+        _dizzyTime = DizzyTimeCounter;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (_countTimer <= 0)
+        if (_dizzyTime < 0)
         {
-            if (!_lastFireWorkSpawned)
-            {
-                if (_fireworkCounter <= 2)
-                {
-                    _fireworkCounter++;
-                    Instantiate(Fireworks[Random.Range(0, 2)]);
-                }
-                else
-                {
-                    Instantiate(FinishingFirework);
-                    _lastFireWorkSpawned = true;
-                }
-            }
-            else
-            {
-                animator.SetBool("Fall", true);
-            }
-            _countTimer = StartCountTimer;
+
         }
-        else
-        {
-            _countTimer -= Time.deltaTime;
-        }
-        
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
