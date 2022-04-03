331a5d07bd7a2248166d3c5be6d67b79d1a51635
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-04-03 15:37:06 +0200

    badguy killing from front fix

---
+++ Assets/BadGuyBackColl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadGuyBackColl : MonoBehaviour
+{
+    public Kark BadGuy;
+    private PlayerMovement _player;
+
+    private void Awake()
+    {
+        _player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((BadGuy.facingRight == _player.facingRight && _player.isSliding) || _player.isCharging)
+        {
+            StartCoroutine(BadGuy.Die());
+        }
+    }
+}
