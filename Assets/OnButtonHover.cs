d40eb422e5aa819a7c226ae743458969382f5159
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2022-04-11 23:55:00 +0200

    button highlights, canvas ui change, level 1 part 1 ui change, menu fixes

--- Assets/OnButtonHover.cs
+++ Assets/OnButtonHover.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class OnButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class OnButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, ISelectHandler, IDeselectHandler
 {
     public Image imgToMove;
-    private Vector2 _startPos;
-    private Vector2 _endPos;
+    private Vector2 _startPos = new Vector2(-7, 10);
+    private Vector2 _endPos = new Vector2(0, 0);
     private void Start()
     {
-        _startPos = new Vector2(0, 0);
-        _endPos = new Vector2(-7, 10);
+        Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -26,6 +26,21 @@ public class OnButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     }
 
     public void OnPointerClick(PointerEventData eventData)
+    {
+        imgToMove.rectTransform.anchoredPosition = _startPos;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        imgToMove.rectTransform.anchoredPosition = _endPos;
+    }
+    
+    private void TaskOnClick()
+    {
+        imgToMove.rectTransform.anchoredPosition = _startPos;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
     {
         imgToMove.rectTransform.anchoredPosition = _startPos;
     }
