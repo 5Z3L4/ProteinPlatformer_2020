882b780cd39acb598505f5f508383298d8f28ed9
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-04-15 12:49:02 +0200

    sec attack

--- Assets/SECController.cs
+++ Assets/SECController.cs
@@ -10,6 +10,7 @@ public class SECController : MonoBehaviour
     private GameObject _right;
     private Animator _anim;
     private int _shotCOunter = 0;
+    private int _hitCOunter = 0;
     private void Awake()
     {
         _anim = GetComponent<Animator>();
@@ -25,6 +26,7 @@ public class SECController : MonoBehaviour
 
     public void ChoseSide()
     {
+        if (_shotCOunter > 3) return;
         if (Random.Range(0,2) > 0)
         {
             _anim.SetBool("Left", true);
@@ -34,11 +36,19 @@ public class SECController : MonoBehaviour
             _anim.SetBool("Right", true);
         }
         _shotCOunter++;
-        if (_shotCOunter >=3)
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("DogEnemy"))
         {
-            //zrzuæ œmiecia
-            _shotCOunter = 0;
-            _anim.SetBool("Fall", true);
+            _hitCOunter++;
+            if (_hitCOunter >= 3)
+            {
+                //zrzuæ œmiecia
+                _shotCOunter = 0;
+                _anim.SetBool("Fall", true);
+            }
         }
     }
 }
