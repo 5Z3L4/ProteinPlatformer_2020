feb8c09f68f04d6c260f92c1139f5f948031c18d
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-04-27 01:08:56 +0200

    WholeLevel scores WholeGame scores

--- Assets/LeaderboardsController.cs
+++ Assets/LeaderboardsController.cs
@@ -18,23 +18,68 @@ public class LeaderboardsController : MonoBehaviour
     {
         _db.GetToDBAllUsersPlease(levelName + "_1");
     }
-    //take scores for 1/3 of level
-    public List<ScoresPlayers> GetLevelScores(string levelName)
+
+    //take scores for 1/3 of level Level1_1 etc
+    public List<ScoresPlayers> GetLevelPartScores(string levelName)
     {
         List<ScoresPlayers> scores = new List<ScoresPlayers>();
-        foreach (var score in _db.scores)
+        List<ScoreData> tempScoreData = _db.scores;
+        foreach (var score in tempScoreData)
         {
             scores.Add(new ScoresPlayers 
             { 
                 playerName = score.playerName,
                 wallet = score.wallet,
-                score = score.levels.Where(x => x.levelName == levelName).Select(p => p.score).SingleOrDefault()
+                score = score.levels.Where(x => x.levelName == levelName).FirstOrDefault().score
             });
         }
         return scores.OrderByDescending(p => p.score).ToList();
     }
 
-    //summary of 3 parts of level
+    //summary of 3 parts of level Level1 etc
+    public List<ScoresPlayers> GetWholeLevelScores(string levelName)
+    {
+        List<ScoresPlayers> scores = new List<ScoresPlayers>();
+        foreach (var score in _db.scores)
+        {
+            int tempScore = 0;
+            foreach (var level in score.levels)
+            {
+                if (levelName.Contains(levelName))
+                {
+                    tempScore += level.score;
+                }
+            };
+
+            scores.Add(new ScoresPlayers
+            {
+                playerName = score.playerName,
+                wallet = score.wallet,
+                score = tempScore
+            });
+        }
+        return scores;
+    }
 
     //summary of all levels
+    public List<ScoresPlayers> GetWholeGameScores()
+    {
+        List<ScoresPlayers> scores = new List<ScoresPlayers>();
+        foreach (var score in _db.scores)
+        {
+            int tempScore = 0;
+            foreach (var level in score.levels)
+            {
+                tempScore += level.score;
+            };
+
+            scores.Add(new ScoresPlayers
+            {
+                playerName = score.playerName,
+                wallet = score.wallet,
+                score = tempScore
+            });
+        }
+        return scores;
+    }
 }
