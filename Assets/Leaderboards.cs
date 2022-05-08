aaa056e26af34f809b4bc48e6849a10cd90b8075
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2022-05-08 20:10:23 +0200

    leaderboards thropees

--- Assets/Leaderboards.cs
+++ Assets/Leaderboards.cs
@@ -10,6 +10,7 @@ public class Leaderboards : MonoBehaviour
     public RectTransform template;
     public RectTransform contentHolder;
     public List<GameObject> playerRecords = new List<GameObject>();
+    public Sprite[] thropiees;
     [SerializeField]
     private LeaderboardsController _LC;
     private SaveManager _SM;
@@ -35,14 +36,22 @@ public class Leaderboards : MonoBehaviour
                     yourScore.transform.GetChild(2).gameObject.GetComponent<TMP_Text>().SetText(record.playerName);
                     yourScore.transform.GetChild(3).gameObject.GetComponent<TMP_Text>().SetText(record.score.ToString());
                 }
+                
                 GameObject template = Instantiate(this.template.gameObject, contentHolder);
                 template.SetActive(true);
+                if (pos < 17)
+                {
+                    GameObject thropeeImg = template.transform.GetChild(0).transform.GetChild(0).gameObject;
+                    thropeeImg.SetActive(true);
+                    thropeeImg.GetComponent<Image>().sprite = thropiees[pos-1];
+                }
                 TMP_Text position = template.transform.GetChild(0).gameObject.GetComponent<TMP_Text>();
                 TMP_Text name = template.transform.GetChild(1).gameObject.GetComponent<TMP_Text>();
                 TMP_Text playerScore = template.transform.GetChild(2).gameObject.GetComponent<TMP_Text>();
                 position.SetText(pos + ". ");
                 name.SetText(record.playerName);
                 playerScore.SetText(record.score.ToString());
+                
                 pos++;
                 playerRecords.Add(template);
             }
